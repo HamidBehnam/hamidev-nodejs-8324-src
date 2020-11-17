cb0875af9025fa740cac66eb0efd2a08cc3356fb
HamidBehnam <hamid.behnam@gmail.com>
2020-11-16 21:42:58 -0800

    convert the project model to the mongoose model

--- src/projects/models/projects.model.ts
+++ src/projects/models/projects.model.ts
@@ -1,6 +1,27 @@
-export const getProjects = async () => {
-    return {
-        name: "Hamid",
-        code: "ASDF458"
-    };
-};
+import {Document, model, Model, Schema} from "mongoose";
+
+interface IProject extends Document {
+    projectName: string;
+    projectCode: string;
+}
+
+const ProjectSchema: Schema = new Schema<any>({
+    projectName: {
+        type: String,
+        required: true
+    },
+    projectCode: {
+        type: String,
+        required: true
+    }
+});
+
+export const Project: Model<IProject> = model('Project', ProjectSchema);
+
+
+// export const getProjects = async () => {
+//     return {
+//         name: "Hamid",
+//         code: "ASDF458"
+//     };
+// };

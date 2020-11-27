126ebce24b163972faba883486fc8a1aa1a8f9e9
HamidBehnam <hamid.behnam@gmail.com>
2020-11-26 16:55:51 -0800

    renaming the frontend-angular app to angular-sample-app

--- frontend-samples/angular-sample-app/karma.conf.js
+++ frontend-samples/angular-sample-app/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend-angular'),
+      dir: require('path').join(__dirname, './coverage/angular-sample-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },

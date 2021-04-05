cb5a637a6259d5db2ab0df036cfd7dbf65a3d706
Hamid Behnam <hamidbehnam@Hamids-MacBook-Pro-2.local>
2021-04-05 13:27:31 -0700

    using path module to provide the path to the log directory

--- src/common/services/winston.service.ts
+++ src/common/services/winston.service.ts
@@ -1,4 +1,5 @@
 import winston from 'winston'
+import path = require('path');
 
 class WinstonService {
     private levels = {
@@ -28,10 +29,12 @@ class WinstonService {
     private transports = [
         new winston.transports.Console(),
         new winston.transports.File({
-            filename: 'logs/error.log',
+            filename: path.resolve(__dirname, '../logs/error.log'),
             level: 'error',
         }),
-        new winston.transports.File({ filename: 'logs/all.log' }),
+        new winston.transports.File({
+            filename: path.resolve(__dirname, '../logs/all.log')
+        }),
     ]
 
     constructor() {
